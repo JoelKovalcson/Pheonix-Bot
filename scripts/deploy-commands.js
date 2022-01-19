28df06d88f1e21084d18aaaab970ee40732d0192
Joel Kovalcson <joelkovalcson@gmail.com>
2022-01-19 07:32:59 -0500

    Added console logging for each command registered

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { commandsJSON } = require('../commands');
-require('dotenv').config();
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
 rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commandsJSON })
-	.then(() => console.log('Commands deployed.'))
+	.then((response) => {
+		// Log each command name and id to console (Might store this for populating selections later)
+		for(var command of response) {
+			console.log(`${command.name}: ${command.id}`);
+		}
+		console.log('Commands deployed.');
+	})
 	.catch(console.error);
