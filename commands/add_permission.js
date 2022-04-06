baf40a1ee45d197112c545a3a249e5739914183f
Joel Kovalcson <joelkovalcson@gmail.com>
2022-04-06 05:55:30 -0400

    Renamed files

---
+++ commands/add_permission.js
@@ -0,0 +1,50 @@
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const { Role, GuildMember } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('addperm')
+		.setDescription('Set whether a user or role can use a specific command')
+		.setDefaultPermission(true)
+		.addMentionableOption(option => option.setName('target').setDescription('The user or role').setRequired(true))
+		.addStringOption(option => option.setName('command').setDescription('The command to give use permission').setRequired(true))
+		.addBooleanOption(option => option.setName('perm').setDescription('Whether to give or revoke permission').setRequired(true)),
+	async execute(interaction) {
+		const mentionable = interaction.options.getMentionable('target');
+		const command_name = interaction.options.getString('command');
+		const perm_set = interaction.options.getBoolean('perm');
+		const command = (await interaction.client.guilds.cache.get(interaction.guildId).commands.fetch()).find((command) => command.name === command_name);
+		let perms;
+		let ping;
+
+		if(!command) {
+			interaction.reply('Command name not found.');
+			return;
+		}
+		
+		if (mentionable instanceof Role) {
+			if (interaction.client.guilds.cache.get(interaction.guildId).roles.everyone.id == mentionable.id) ping = `@everyone`
+			else ping = `<@&${mentionable.id}>`;
+			perms = {
+				id: mentionable.id,
+				type: 'ROLE',
+				permission: perm_set
+			};
+		}
+		else if (mentionable instanceof GuildMember) {
+			ping = `<@${mentionable.id}>`;
+			perms = {
+				id: mentionable.id,
+				type: 'USER',
+				permission: perm_set
+			};
+		}
+		else {
+			interaction.reply('Invalid Role or User provided.');
+			return;
+		}
+		
+		command.permissions.add({permissions: [perms]})
+			.then(interaction.reply({content:`Permission for \`${command_name}\` turned ${(perm_set) ? 'on' : 'off'} for ${ping}`, ephemeral: true}));
+	}
+}
---
+++ commands/list_permission.js
@@ -0,0 +1,55 @@
+const {	SlashCommandBuilder } = require('@discordjs/builders');
+const {	Role, GuildMember, MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('listperm')
+		.setDescription('List all permissions for a command.')
+		.setDefaultPermission(true)
+		.addStringOption(option => option.setName('command').setDescription('The command to list permissions').setRequired(true)),
+	async execute(interaction) {
+		const command_name = interaction.options.getString('command');
+		// Might look into seeing if there's a better way of doing this? Feels clunky
+		const command = (await interaction.client.guilds.cache.get(interaction.guildId).commands.fetch()).find((command) => command.name === command_name);
+
+		if (!command) {
+			interaction.reply('Command name not found.');
+			return;
+		}
+
+		let perms;
+		// If permissions don't exist for this command, set undefined
+		try {
+			perms = await command.permissions.fetch();
+		}
+		catch {
+			perms = undefined;
+		}
+
+		// Create embed displaying default behavior for command
+		const em = new MessageEmbed()
+			.setTitle(command_name)
+			.setDescription(`Default permission: \`${(command.defaultPermission) ? 'on' : 'off'}\``);
+
+		// If permissions exist, fill out role & user restrictions
+		if (perms) {
+			let roles = '';
+			let users = '';
+			for (var perm of perms) {
+				if (perm.type == 'ROLE') {
+					if (perm.id == interaction.client.guilds.cache.get(interaction.guildId).roles.everyone.id) roles += ` @everyone`
+					else roles += ` <@&${perm.id}>`
+				}
+				else if (perm.type == 'USER') users += ` <@${perm.id}>`;
+			}
+			// If role or users existed, add them to embed
+			if(roles) em.addField('Roles', roles, false);
+			if(users) em.addField('Users', users, false);
+		}
+		
+
+		interaction.reply({
+			embeds: [em]
+		})
+	}
+}
