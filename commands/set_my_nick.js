01367f9a7b7231d66cd2ade967307d9c25dff8f8
Joel Kovalcson <joelkovalcson@gmail.com>
2022-09-10 14:35:10 -0500

    Updated to Discord.js V14

--- commands/set_my_nick.js
+++ commands/set_my_nick.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { Embed } = require('discord.js');
 const { getVisitorRole } = require('../util/get-roles');
 
 module.exports = {
@@ -7,27 +7,34 @@ module.exports = {
 		.setName('setmynick')
 		.setDescription('Sets the nickname of the user to the string provided')
 		.addStringOption(option => option.setName('nick').setDescription('The nickname for the user').setRequired(true))
-		.setDefaultPermission(false),
+		.setDefaultMemberPermissions('0'),
 	async execute(interaction) {
 		const nick = interaction.options.getString('nick');
 
-		const logMessage = new MessageEmbed()
+		const logMessage = new EmbedBuilder()
 			.setTitle('setmynick')
 			.setDescription(`Used by: <@!${interaction.user.id}>`);
 
 		// This should work as long as the nickname isn't too long and the bot has perms higher than the user.
 		try {
 			await interaction.member.setNickname(nick);
+
 			const visitor_role = await getVisitorRole(interaction.client.guilds.cache.get(interaction.guildId));
+			
 			interaction.member.roles.remove(process.env.LOCKED_NICKNAME_ID);
 			interaction.member.roles.add(visitor_role);
 			interaction.reply({content:`Successfully set nickname to: ${nick}`, ephemeral: true});
-			return {embeds: [logMessage.addField('Success', `<@!${interaction.member.id}> has changed nickname to \`${nick}\`.`, false)]};		
+			
+			logMessage.addField('Success', `<@!${interaction.member.id}> has changed nickname to \`${nick}\`.`, false);
+			return {embeds: [logMessage.data]};
 		}
 		catch (err) {
 			console.log(err);
+
 			interaction.reply({content:`An error occurred setting nickname to: ${nick}`, ephemeral: true});
-			return {embeds: [logMessage.addField('Failed', `Error setting nickname to ${nick}`, false)]}
+			
+			logMessage.addField('Failed', `Error setting nickname to ${nick}`, false);
+			return {embeds: [logMessage.data]}
 		}
 	}
 }
