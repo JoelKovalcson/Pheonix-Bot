70ebdc8be332f63a5ed81662678bd65ed654dad4
Joel Kovalcson <joelkovalcson@gmail.com>
2022-09-11 10:39:25 -0500

    Added more channels and roles required

--- util/get-channels.js
+++ util/get-channels.js
@@ -2,4 +2,8 @@ async function getLogChannel (guild) {
 	return guild.channels.fetch(`${process.env.LOG_CHANNEL_ID}`);
 }
 
-module.exports = {getLogChannel};
+async function getJoinClanChannel (guild) {
+	return guild.channels.fetch(`${process.env.JOIN_CLAN_CHANNEL_ID}`);
+}
+
+module.exports = {getLogChannel, getJoinClanChannel};
--- util/get-roles.js
+++ util/get-roles.js
@@ -1,13 +1,42 @@
 async function getVisitorRole(guild) {
-	return guild.roles.fetch(`${process.env.VISITOR_ROLE_ID}`);
+	return guild.roles.cache.find(role => role.id == process.env.VISITOR_ROLE_ID);
 }
 
 async function getMemberRole(guild) {
-	return guild.roles.fetch(`${process.env.MEMBER_ROLE_ID}`);
+	return guild.roles.cache.find(role => role.id == process.env.MEMBER_ROLE_ID);
 }
 
 async function getLeadershipRole(guild) {
-	return guild.roles.fetch(`${process.env.LEADERSHIP_ROLE_ID}`);
+	return guild.roles.cache.find(role => role.id == process.env.LEADERSHIP_ROLE_ID);
 }
 
-module.exports = { getVisitorRole, getMemberRole, getLeadershipRole };
+async function getRecruitRole(guild) {
+	return guild.roles.cache.find(role => role.id == process.env.RECRUIT_ROLE_ID);
+}
+
+async function getRecruiterRole(guild) {
+	return guild.roles.cache.find(role => role.id == process.env.RECRUITER_ROLE_ID);
+}
+
+async function getHeadRecruiterRole(guild) {
+	return guild.roles.cache.find(role => role.id == process.env.HEAD_RECRUITER_ROLE_ID);
+}
+
+async function getAssociateRole(guild) {
+	return guild.roles.cache.find(role => role.id == process.env.ASSOCIATE_ROLE_ID);
+}
+
+async function getGuestRole(guild) {
+	return guild.roles.cache.find(role => role.id == process.env.GUEST_ROLE_ID);
+}
+
+module.exports = { 
+	getVisitorRole,
+	getMemberRole,
+	getLeadershipRole, 
+	getRecruiterRole, 
+	getHeadRecruiterRole, 
+	getRecruitRole, 
+	getAssociateRole,
+	getGuestRole
+};
