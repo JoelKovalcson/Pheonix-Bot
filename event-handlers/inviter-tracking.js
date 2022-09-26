5416cdc3ca18d27f62ba5b6acfbb70b79b46a59a
Joel Kovalcson <joelkovalcson@gmail.com>
2022-09-26 04:27:56 -0500

    Included other invite codes in tracking

--- event-handlers/inviter-tracking.js
+++ event-handlers/inviter-tracking.js
@@ -2,28 +2,39 @@ const { EmbedBuilder, Colors } = require("discord.js");
 const { logMessage } = require("../util/log");
 
 // These should be somewhere else...
-const inviteTable = {
-	'S2YQD23FcC': '322570144520077312',
-	'Qz8yvBgMcq': '674728562799935508',
-	'ntp9E86ZvR': '376397312257294336',
-	'uE5MfSGygt': '641995975191429140',
-	'XGUVeaBK2C': '265599878460080128',
-	'jN496qVWHu': '475055012469538826',
-	'SRjc4TaQsE': '293457931024859136'
+const recruiterInviteTable = {
+	'S2YQD23FcC': '<@322570144520077312>',
+	'Qz8yvBgMcq': '<@674728562799935508>',
+	'ntp9E86ZvR': '<@376397312257294336>',
+	'uE5MfSGygt': '<@641995975191429140>',
+	'XGUVeaBK2C': '<@265599878460080128>',
+	'jN496qVWHu': '<@475055012469538826>',
+	'SRjc4TaQsE': '<@293457931024859136>'
+}
+
+const publicInviteTable = {
+	'2eCcCZ44Rt': 'Warframe Forum',
+	'2t7ZRyaqSV': 'Warframe Fandom',
+	'vykXbTWuP6': 'Warframe Reddit'
 }
 
 var inviteCounts = {};
 
 const logInvites = async (inviteCode, member) => {
+	let inviteSource = recruiterInviteTable[inviteCode];
+	
 	// Edge case for other invites
-	if (inviteTable[inviteCode] === undefined) return;
+	if (inviteSource === undefined) {
+		inviteSource = publicInviteTable[inviteCode];
+		if (inviteSource === undefined) return;
+	}
 	
 	const msgEm = new EmbedBuilder()
 		.setTitle('User Joined Via Invite')
 		.setFields(
 			{
 				name: `https://discord.gg/${inviteCode}`,
-				value: `<@${member.id}> has joined with <@${inviteTable[inviteCode]}>'s invite code!`
+				value: `<@${member.id}> has joined with ${inviteSource}'s invite code!`
 			}
 		)
 		.setColor(Colors.DarkOrange)
